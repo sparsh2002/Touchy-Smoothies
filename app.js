458d12062e9c1ee9e69005f4bfb442950ae1e076
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-05-31 17:51:07 +0530

    cookies initialized

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv')
 const authRoutes = require('./routes/authRoutes')
 dotenv.config()
 const app = express();
-
+const cookieParser = require('cookie-parser')
 // middleware
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -26,3 +27,17 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+
+app.get('/set-cookies' , (req , res) =>{
+  // res.setHeader('Set-Cookie' , 'newUser=true')
+  res.cookie('newUser' , false)
+  res.cookie('isEmployee' , true , {maxAge: 1000*60*60*24})
+  res.send('you got the cookies')
+})
+
+app.get('/read-cookies' , (req, res)=>{
+  const cookies = req.cookies;
+  console.log(cookies)
+  res.json(cookies)
+})
