940dc903fb24aba4fb8dea70331603e739905d13
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-05-31 15:39:09 +0530

    frontend design and database connected

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = process.env.URI
+const PORT = process.env.PORT
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    app.listen(PORT)
+    console.log(`listening on PORT ${PORT}`)
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
